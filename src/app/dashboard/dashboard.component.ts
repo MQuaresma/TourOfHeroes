7fb1429338f6d3d0c503accaca2431d54da23c14
MQuaresma <miguelquaresma97@gmail.com>
2018-08-02 15:51:27 +0100

    Dashboard implemented via Angular grid

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,22 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
 import { HeroService } from '../hero.service';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
-  heroes: Hero[] = [];
+    heroes: Hero[] = [];
+    menuName = 'Hero Dashboard';
 
-  constructor(private heroService: HeroService) { }
+    constructor(private heroService: HeroService) { }
 
-  ngOnInit() {
-    this.getHeroes();
-  }
+    ngOnInit() {
+        this.getHeroes();
+    }
 
-  getHeroes(): void{
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes.slice(1, 5));
-  }
+    getHeroes(): void{
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    }
 }
